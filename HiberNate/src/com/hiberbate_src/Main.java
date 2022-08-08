0ad099d3069c0d1f9e5c1b22ea967b72d3145c81
Emircan Dayac <70799758+EmircanDayac@users.noreply.github.com>
2022-08-08 12:33:42 +0300

    Update Main.java

--- HiberNate/src/com/hiberbate_src/Main.java
+++ HiberNate/src/com/hiberbate_src/Main.java
@@ -12,6 +12,8 @@ import org.hibernate.cfg.*;
 
 public class Main {
 
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SessionFactory _ses = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(City.class).buildSessionFactory();
@@ -20,12 +22,25 @@ public class Main {
 		try {
 			 _ses_.beginTransaction();
 
-			 List<City> Cities =  extracted(_ses_);
-			 for(City city_x:Cities ) {
-
-				 System.out.println(city_x.getName());
-			 }
+//			 List<City> Cities =  extracted(_ses_);
+//			 for(City city_x:Cities ) {
+//
+//				 System.out.println(city_x.getName());
+			 
+			 City city  = _ses_.get(City.class, 22333);
+//			 city.setId(22333);
+//			 city.setName("a");
+//			 city.setInfo("a");
+//			 city.setDistrict("a");
+//			 city.setCountryCode("a");
+			 
+			 System.out.println(city.getName());
+//			 _ses_.save(city);
+			 _ses_.delete(args);
 			 _ses_.getTransaction().commit();
+			 System.out.println("sehir eklendi");
+//			 }
+		
 			
 		}finally {
 			_ses.close();
@@ -34,8 +49,8 @@ public class Main {
 
 	}
 
-	private static List extracted(Session _ses_) {
-		return _ses_.createQuery("from City").getResultList();
-	}
+//	private static List extracted(Session _ses_) {
+//		return _ses_.createQuery("from City c where c.Name = 'Seattle'").getResultList();
+//	}
 
 }
